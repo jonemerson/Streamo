d03208b9a147998929e58e41ced83af38bdb2cb6
jonemerson <jon@jonemerson.net>
2017-02-20 19:35:19 -0800

    Initial commit: Blank project

---
+++ Streamo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Streamo
+//
+//  Created by Jonathan Emerson on 2/20/17.
+//  Copyright © 2017 Jon Emerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
